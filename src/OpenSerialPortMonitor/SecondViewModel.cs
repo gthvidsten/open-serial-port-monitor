6a17b0f79c74ebe085598579035c57beb35197d0
gthvidsten <github.com@whitestone.no>
2016-01-18 13:35:05 +0100

    Added project files

---
+++ src/OpenSerialPortMonitor/SecondViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Whitestone.OpenSerialPortMonitor.Main
+{
+    public class SecondViewModel : PropertyChangedBase
+    {
+        public void SecondButton()
+        {
+            MessageBox.Show("Second box");
+        }
+    }
+}
---
+++ src/OpenSerialPortMonitor/ShellViewModel.cs
@@ -0,0 +1,56 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Whitestone.OpenSerialPortMonitor.Main
+{
+    public class ShellViewModel : Screen
+    {
+        string name;
+        SecondViewModel _secondView;
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = value;
+                NotifyOfPropertyChange(() => Name);
+                NotifyOfPropertyChange(() => CanSayHello);
+            }
+        }
+
+        public bool CanSayHello
+        {
+            get { return !string.IsNullOrWhiteSpace(Name); }
+        }
+
+        public void SayHello()
+        {
+            MessageBox.Show(string.Format("Hello {0}!", Name)); //Don't do this in real life :)
+        }
+
+        protected override void OnInitialize()
+        {
+            SecondView = new SecondViewModel();
+            base.OnInitialize();
+        }
+
+        public SecondViewModel SecondView
+        {
+            get
+            {
+                return _secondView;
+            }
+            set
+            {
+                _secondView = value;
+                NotifyOfPropertyChange(() => SecondView);
+            }
+        }
+    }
+}
