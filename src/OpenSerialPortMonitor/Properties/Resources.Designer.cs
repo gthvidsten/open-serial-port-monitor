865977755b65e9dbbb74cc7840cb7f9c48a2f179
gthvidsten <github.com@whitestone.no>
2016-01-22 15:06:26 +0100

    Modified how application icon is used

--- src/OpenSerialPortMonitor/Properties/Resources.Designer.cs
+++ src/OpenSerialPortMonitor/Properties/Resources.Designer.cs
@@ -59,5 +59,15 @@ namespace Whitestone.OpenSerialPortMonitor.Main.Properties {
                 resourceCulture = value;
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
+        internal static System.Drawing.Icon ApplicationIcon {
+            get {
+                object obj = ResourceManager.GetObject("ApplicationIcon", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
     }
 }
