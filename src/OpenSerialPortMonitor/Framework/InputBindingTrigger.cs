a0bbc23a62a5940cc82ed1c21c562a5e35f28ebb
Glenn Thomas Hvidsten <gthvidsten@whitestone.no>
2016-01-30 13:22:58 +0100

    Added missing file

---
+++ src/OpenSerialPortMonitor/Framework/InputBindingTrigger.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Interactivity;
+
+namespace Whitestone.OpenSerialPortMonitor.Main.Framework
+{
+    public class InputBindingTrigger:TriggerBase<FrameworkElement>,ICommand
+    {
+        public InputBindingTrigger()
+        {
+
+        }
+        public InputBinding InputBinding
+        {
+            get { return (InputBinding)GetValue(InputBindingProperty); }
+            set { SetValue(InputBindingProperty, value); }
+        }
+        public static readonly DependencyProperty InputBindingProperty =
+            DependencyProperty.Register("InputBinding", typeof(InputBinding)
+            , typeof(InputBindingTrigger)
+            , new UIPropertyMetadata(null));
+        protected override void OnAttached()
+        {
+            if (InputBinding != null)
+            {
+                InputBinding.Command = this;
+                AssociatedObject.InputBindings.Add(InputBinding);
+            }
+            base.OnAttached();
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            // action is anyway blocked by Caliburn at the invoke level
+            return true;
+        }
+        public event EventHandler CanExecuteChanged = delegate { };
+
+        public void Execute(object parameter)
+        {
+            InvokeActions(parameter);
+        }
+    }
+}
